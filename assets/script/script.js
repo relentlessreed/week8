44df8133414d33abbabf925e85ebd598a1249d23
Ed <relentlessreed@gmail.com>
2021-05-07 11:34:15 -0500

    Added style tweaks/ Resume Button

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,15 @@
 // Header + HERO (Edward Reed Full-Stack Web Developer)
 // Navbar (Home | Skills | Projects | Contact | Resume)
+$("#gitHub").click(function (window.open("http://www.github.com/relentlessreed/", "_blank"));
+$("#linkedIn").click(function () {
+  $("#target").click();
+});
+$("#email").click(function () {
+  $("#target").click();
+});
+$("#resumé").click(function () {
+  $("#target").click();
+});
 // Skills (Communication, Collaboration, Problem Solving, Technologies and Languages Learned:_)
 // Projects (What Can I Cook That Won't Kill Me, Weather Wizard, Workday Scheduler)
 // Contact (Email, LinkedIn, GitHub, )
